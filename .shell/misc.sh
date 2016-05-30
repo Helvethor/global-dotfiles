76aa91c1fd9569827ec11aecaa2b1cffbd0d7104
Vincent Pasquier <helvethor@debian>
2016-05-30 15:31:50 +0200

    ...

--- .shell/misc.sh
+++ .shell/misc.sh
@@ -4,5 +4,5 @@ source ~/bin/acd_func.sh
 export EDITOR=vim
 
 if ! echo "$LD_LIBRARY_PATH" | grep -q '/usr/local/lib'; then
-	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+	export LD_LIBRARY_PATH=/usr/local/lib
 fi
