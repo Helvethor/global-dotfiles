7ab985bd849a98644b979653acbc99017ae98e57
Vincent Pasquier <vincentpasquier@posteo.net>
2016-06-26 20:30:53 +0200

    Welcome to Arch

---
+++ src/misc/git-submodule-add-aur.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+HOME_DIR=~
+AUR_DIR=~/src/aur
+AUR_PROJECTS=$(ls $AUR_DIR)
+
+for project in $AUR_PROJECTS; do
+	if ! grep -q "$project" ~/.gitmodules; then
+
+		project_dir=$AUR_DIR/$project
+		url=$(cat $project_dir/.git/config | grep "url")
+		path=${project_dir#$HOME_DIR/}
+
+		printf "[submodule \"$path\"]\n" >> $HOME_DIR/.gitmodules
+		printf "\tpath = $path\n" >> $HOME_DIR/.gitmodules
+		printf "$url\n" >> $HOME_DIR/.gitmodules
+
+	fi
+done
