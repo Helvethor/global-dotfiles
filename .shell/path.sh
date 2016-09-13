648d2881b3f5b97ec29c6f6586356a22d1124e24
Vincent Pasquier <vincentpasquier@posteo.net>
2016-09-13 18:42:22 +0200

    new aur

--- .shell/path.sh
+++ .shell/path.sh
@@ -1,6 +1,6 @@
 # Paths, includes /opt/xxx/bin
 
-PATH=/bin:/sbin/:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/src/script
+PATH=/bin:/sbin/:/usr/bin:/usr/sbin:/usr/bin/core_perl:/usr/local/bin:/usr/local/sbin:~/src/script
 
 for optPath in $(find /opt/ -maxdepth 3 -name bin); do 
 	PATH=$PATH:$optPath;
