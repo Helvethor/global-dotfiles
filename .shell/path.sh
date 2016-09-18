f80947805755ed0d9bee49fda8ae37134d2b45c2
Vincent Pasquier <vincentpasquier@posteo.net>
2016-09-18 09:52:10 +0200

    ..

--- .shell/path.sh
+++ .shell/path.sh
@@ -2,7 +2,7 @@
 
 PATH=/bin:/sbin/:/usr/bin:/usr/sbin:/usr/bin/core_perl:/usr/local/bin:/usr/local/sbin:~/src/script
 
-for optPath in $(find /opt/ -maxdepth 3 -name bin); do 
+for optPath in $(find /opt/ -maxdepth 2 -name bin); do 
 	PATH=$PATH:$optPath;
 done
 
