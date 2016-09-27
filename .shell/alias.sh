1f67431b0f0b6fe1d7a3cf1ab057f9b2c130d00b
Vincent Pasquier <vincentpasquier@posteo.net>
2016-09-27 23:02:10 +0200

    revamped aliases

--- .shell/alias.sh
+++ .shell/alias.sh
@@ -1,11 +1,5 @@
 # Aliases
 
 alias shut='sudo poweroff'
-
-alias sshroot='ssh root@vincentpasquier.net'
-alias sshsv1='ssh root@sv1.lan'
-alias sshsv2='ssh root@sv2.lan'
-
 alias gitfetch='git fetch --all && git reset --hard origin/master'
-
 alias diff-x='diff --suppress-common-lines --tabsize 4 -W $(tput cols) -y'
