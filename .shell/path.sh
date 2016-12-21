55e40f6065d703adb966823cc1210c25b7ed59f0
Vincent Pasquier <vincentpasquier@posteo.net>
2016-12-21 14:47:50 +0100

    ..

--- .shell/path.sh
+++ .shell/path.sh
@@ -1,4 +1,4 @@
 # Paths
 
-PATH=/bin:/sbin/:/usr/bin:/usr/sbin:/usr/bin/core_perl:/usr/local/bin:/usr/local/sbin:~/src/script
+PATH=/bin:/sbin/:/usr/bin:/usr/sbin:/usr/bin/core_perl:/usr/local/bin:/usr/local/sbin:~/src/script:/opt/android-sdk/platform-tools/
 export PATH
