1ec0acb6a9a2a819ee828f49960a537e1306e403
Vincent Pasquier <vincentpasquier@posteo.net>
2017-03-07 23:27:50 +0100

    ..

--- .shell/alias.sh
+++ .shell/alias.sh
@@ -3,3 +3,4 @@
 alias shut="echo Y\'a sure \'bout that\? && read && sudo systemctl poweroff"
 alias gitfetch='git fetch --all && git reset --hard origin/master'
 alias diff-x='diff --suppress-common-lines --tabsize 4 -W $(tput cols) -y'
+alias gitcreate="curl -u '$1' https://api.github.com/user/repos -d '{\"name\":\"$2\"}'"
